04ee29713f0895a73d4e2284ccc842415d2844de
optus-56 <bijanbakabal45@gmail.com>
2024-06-22 13:48:05 +0545

    name

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setApplicationName("KUvent");
+    a.setApplicationName("KUvents");
     MainWindow w;
     w.setWindowTitle(a.applicationName());
     w.show();
