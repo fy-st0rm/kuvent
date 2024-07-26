1eb380b7075a7e6259fa35065caca69669ed8259
Rasaili-rain <saditrasaili063@gmail.com>
2024-07-26 21:10:13 +0545

    add support for ARCH linux

--- src/utils.h
+++ src/utils.h
@@ -5,10 +5,16 @@
 #include <sstream>
 #include <iomanip>
 
+
+
 static std::time_t dateStringToTimestamp(const std::string& dateStr) {
 	std::tm tm = {};
 	std::istringstream ss(dateStr);
-	ss >> std::get_time(&tm, "%d-%B-%Y"); // Parse string into std::tm
+	#ifdef _WIN32
+        ss >> std::get_time(&tm, "%d-%B-%Y"); // Parse string into std::tm
+    #else
+        ss >> std::get_time(&tm, "%d-%b-%Y"); // Parse string into std::tm
+    #endif
 	if (ss.fail()) {
 		throw std::runtime_error{"failed to parse time string"};
 	}
